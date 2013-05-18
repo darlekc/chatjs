8c9dea2ae4484b1035dc3b6e5148eb6c8954aaea
André Rodrigues Pena <andrerpena@gmail.com>
2013-05-18 19:05:25 -0300

    Favico and license

--- ChatJsSample/ChatJs/Scripts/jquery.chatjs.js
+++ ChatJsSample/ChatJs/Scripts/jquery.chatjs.js
@@ -1,4 +1,22 @@
-﻿// CHAT CONTAINER
+﻿/**
+ * ChatJS 1.0
+ * www.chatjs.net
+ * 
+ * Copyright (c) 2013, André Pena
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ *     - Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ * 
+ *     - The software containing ChatJS must not directly or indirectly charge the
+ *       end user at any plan. Otherwise a commercial license is required.
+ **/
+
+// CHAT CONTAINER
 (function ($) {
     function ChatContainer(options) {
         /// <summary>This is a window container, responsible for hosting both the users list and the chat window </summary>
--- ChatJsSample/ChatJs/Scripts/jquery.chatjs.signalradapter.js
+++ ChatJsSample/ChatJs/Scripts/jquery.chatjs.signalradapter.js
@@ -1,4 +1,22 @@
-﻿function SignalRAdapter() {
+﻿/**
+ * ChatJS 1.0
+ * www.chatjs.net
+ * 
+ * Copyright (c) 2013, André Pena
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ * 
+ *     - Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ * 
+ *     - The software containing ChatJS must not directly or indirectly charge the
+ *       end user at any plan. Otherwise a commercial license is required.
+ **/
+
+function SignalRAdapter() {
     /// <summary>
     /// Adapter SignalR para o ChatJs. In order to use this adapter.. Pass an instance of this 
     /// function to $.chat()
